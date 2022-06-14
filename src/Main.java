b0ca4b8e0dcb05fc7f3921b0db9858409ea7c7f2
Moises Muiña <mochyfm@gmail.com>
2022-06-14 14:06:54 +0100

    Creada interfaz principal en el main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,100 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        boolean out = false;
+        Plane f18 = new Plane(0,"","");
 
-        System.out.println("Hello world!");
+        while (!out) {
 
+            System.out.println("\n");
+            System.out.println("1. Inicializar F18. ");
+            System.out.println("2. Alternar estado de los flaps. ");
+            System.out.println("3. Alternar estado del tren de aterrizaje. ");
+            System.out.println("4. Armar sistema de eyección. ");
+            System.out.println("5. Eyectar piloto. ");
+            System.out.println("Q. Salir");
+            System.out.println("\n");
+            System.out.println("En caso de pulsar cualquier otra opción, si hay un avión ya creado " +
+                    "se mostrará su información.");
+            System.out.println("\n");
+
+            System.out.print("Selección: ");
+
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "1":
+
+                    System.out.print("\n" + "Introduzca el número de litros de combustible cargados: ");
+                    int liters = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Introduzca el apodo del piloto: ");
+                    String nickname = scanner.nextLine();
+
+                    System.out.print("Introduzca el número de escuadrón: ");
+                    String squadNum = scanner.nextLine();
+
+                    f18 = new Plane(liters, nickname, squadNum);
+
+                    System.out.println("\n" + "El avión ha sido asignado: ");
+                    System.out.println(f18);
+
+                    f18.setInitializedState(true);
+
+                    break;
+
+                case "2":
+
+                    if (!f18.getInitializedState()) System.out.println("\n" + Colors.ANSI_RED + "ERROR: No hay un avión todavia creado" + Colors.RESET);
+                    else f18.toggleFlaps(); System.out.println("\n" + "Los flaps están " + f18.stringifyFlapsState() + "\n");
+
+                    break;
+
+                case "3":
+
+                    if (!f18.getInitializedState()) System.out.println("\n" + Colors.ANSI_RED + "ERROR: No hay un avión todavia creado" + Colors.RESET);
+                    else  f18.toggleLandingGear(); System.out.println("\n" + "El sistema de aterrizaje está " + f18.stringifyLandingGear());
+
+                    break;
+
+                case "4":
+
+                    if (!f18.getInitializedState()) System.out.println("\n" + Colors.ANSI_RED + "ERROR: No hay un avión todavia creado" + Colors.RESET);
+                    else if (f18.getEjectionSystem()) System.out.println("El sistema de ejección ya está armado");
+                    else f18.ejectionSystem(); System.out.println("\n" + "El sistema de eyección está: " + f18.stringifyEjectionSystem());
+
+                    break;
+
+                case "5":
+
+                    if (!f18.getInitializedState()) System.out.println("\n" + Colors.ANSI_RED + "ERROR: No hay un avión todavia creado" + Colors.RESET);
+                    else if (!f18.getEjectionSystem()) { System.out.println("\n" + "¡¡ El sistema de ejección está desarmado !!," +
+                            " debe armarlo primero"); }
+                    else { f18.ejectionSystem(); System.out.println("\n" + "El sistema de eyección está: " + f18.stringifyEjectionSystem() + " el piloto ha sido eyectado. "); f18.setSeatOccupation(false); }
+
+                    break;
+
+                case "Q":
+                case "q":
+
+                    out = true;
+
+                    break;
+
+                default:
+
+                    if (!f18.getInitializedState()) System.out.println("\n" + Colors.ANSI_RED + "ERROR: No hay un avión todavia creado" + Colors.RESET);
+                    else System.out.println(f18);
+
+
+
+            }
+
+        }
 
     }
 }
--- src/Plane.java
+++ src/Plane.java
@@ -3,12 +3,13 @@ public class Plane {
     private float fuelLevel;
     private String pilotCallSign;
     private String squadNumber;
-
     private boolean flaps;
     private boolean landingGear;
     private boolean ejectionSystem;
 
     private boolean seatOccupation;
+
+    private boolean initializedState;
 
     public Plane(float fuelLevel, String pilotCallSign, String squadNumber) {
         this.fuelLevel = fuelLevel;
@@ -18,6 +19,7 @@ public class Plane {
         this.landingGear = false;
         this.ejectionSystem = false;
         this.seatOccupation = true;
+        this.initializedState = false;
     }
 
     public void toggleFlaps() {
@@ -32,12 +34,16 @@ public class Plane {
         ejectionSystem = !ejectionSystem;
     }
 
+    public boolean getEjectionSystem() {
+        return ejectionSystem;
+    }
+
     public void setSeatOccupation(boolean pilotSeated) {
         seatOccupation = pilotSeated;
     }
 
     public String stringifyFlapsState() {
-        return flaps ? "Arriba" : "Abajo";
+        return flaps ? Colors.ANSI_GREEN + "Arriba" + Colors.RESET : Colors.ANSI_RED + "Abajo" + Colors.RESET;
     }
 
     public String stringifyLandingGear() {
@@ -45,23 +51,32 @@ public class Plane {
     }
 
     public String stringifyEjectionSystem() {
-        return ejectionSystem ? "Armado" : "Desarmado";
+        return ejectionSystem ? Colors.ANSI_GREEN + "Armado" + Colors.RESET : Colors.ANSI_RED + "Desarmado" + Colors.RESET;
     }
 
     public String stringifySeatOccupation() {
-        return seatOccupation ? "Verdadero" : "Falso";
+        return seatOccupation ? Colors.ANSI_GREEN + "Verdadero" + Colors.RESET : Colors.ANSI_RED + "Falso" + Colors.RESET;
+    }
+
+    public void setInitializedState(boolean state) {
+        initializedState = state;
+    }
+
+    public boolean getInitializedState() {
+        return initializedState;
     }
 
     @Override
     public String toString() {
-        return  " -------------------------------------- " + "\n" +
-                "Plane:" +
-                "\n\t" + "Nivel de Combustible: " + fuelLevel +
-                "\n\t" + "Apodo del piloto: " + pilotCallSign +
-                "\n\t" + "Numero de escuadrón: " + squadNumber +
-                "\n\t" + "Estado de los flaps: " + this.stringifyFlapsState() +
-                "\n\t" + "Estado del tren de aterrizaje: " + this.stringifyLandingGear() +
-                "\n\t" + "Estado del sistema de eyección del piloto: " + this.stringifyEjectionSystem() +
-                "\n\t" + "Asiento ocupado (?): " + this.stringifySeatOccupation();
+        return  "\n" + " -------------------------------------- " + "\n" +
+                "\n" + "Plane:" + "\n" +
+                "\n\t" + Colors.ANSI_CYAN + "Nivel de Combustible: " + Colors.RESET + fuelLevel +
+                "\n\t" + Colors.ANSI_CYAN + "Apodo del piloto: " + Colors.RESET + pilotCallSign + ". " +
+                "\n\t" + Colors.ANSI_CYAN + "Numero de escuadrón: " + Colors.RESET + squadNumber + ". " +
+                "\n\t" + Colors.ANSI_CYAN + "Estado de los flaps: " + Colors.RESET + this.stringifyFlapsState() + ". " +
+                "\n\t" + Colors.ANSI_CYAN + "Estado del tren de aterrizaje: " + Colors.RESET + this.stringifyLandingGear() + ". " +
+                "\n\t" + Colors.ANSI_CYAN + "Estado del sistema de eyección del piloto: " + Colors.RESET + this.stringifyEjectionSystem() + ". " +
+                "\n\t" + Colors.ANSI_CYAN + "Asiento ocupado (?): " + Colors.RESET + this.stringifySeatOccupation() + ". " +
+                "\n\n" + " -------------------------------------- " + "\n";
     }
 }
